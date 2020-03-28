c82a7c3fc1c7457dcbca5e7bc1df616958948022
Shilton <shilton.sistemas@gmail.com>
2020-03-28 15:01:17 -0300

    Component, JSX, props, state and immutability

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Header from './Header';
 
 function App() {
-	return <div className='App'>Shilton</div>;
+	const [counter, setCounter] = useState(0);
+
+	function handleIncrement() {
+		setCounter(counter + 1);
+	}
+
+	return (
+		<div>
+			<Header title='Semana Omnistack'>Counter: {counter}</Header>
+			<button onClick={handleIncrement}>Incrementa</button>
+		</div>
+	);
 }
 
 export default App;
