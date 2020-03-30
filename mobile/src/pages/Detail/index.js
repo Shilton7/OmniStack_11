6c646fe6e929698c0b18747d9c9146d4719ad037
Shilton <shilton.sistemas@gmail.com>
2020-03-29 23:29:49 -0300

    Implementing pagination, consuming api of incidents and details

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -3,15 +3,20 @@ import logoImg from '../../assets/logo.png';
 import styles from './styles';
 import { View, Text, Image, TouchableOpacity, Linking } from 'react-native';
 import { Feather } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import * as MailCompose from 'expo-mail-composer';
 
 export default function Detail() {
+	//Navigation parameters
 	const navigation = useNavigation();
-	const emailSubject = 'Herói do Caso';
-	const emailRecipients = [''];
-	const message = 'Olá APAD, estou entrando em contato';
-	const phoneWhatsapp = '';
+	const route = useRoute();
+	const incident = route.params.incident;
+
+	//Contact details
+	const emailSubject = `Herói do Caso ${incident.title}`;
+	const emailRecipients = [incident.email];
+	const message = `Olá ${incident.name}, estou entrando em contato sobre o caso ${incident.title}`;
+	const phoneWhatsapp = incident.whatsapp;
 
 	function navigateToIncident() {
 		navigation.goBack();
@@ -35,19 +40,26 @@ export default function Detail() {
 				<Image source={logoImg} />
 
 				<TouchableOpacity onPress={navigateToIncident}>
-					<Feather name='arrow-left' size={16} color='#E02041' />
+					<Feather name='arrow-left' size={30} color='#E02041' />
 				</TouchableOpacity>
 			</View>
 
 			<View style={styles.incident}>
 				<Text style={[styles.incidentProperty, { marginTop: 0 }]}>ONG:</Text>
-				<Text style={styles.incidentValue}>APAD</Text>
+				<Text style={styles.incidentValue}>
+					{incident.name} de {incident.city}/{incident.uf}
+				</Text>
 
 				<Text style={styles.incidentProperty}>CASO:</Text>
-				<Text style={styles.incidentValue}>Cadelinha Diana</Text>
+				<Text style={styles.incidentValue}>{incident.title}</Text>
 
 				<Text style={styles.incidentProperty}>Valor</Text>
-				<Text style={styles.incidentValue}>R$ 300,00</Text>
+				<Text style={styles.incidentValue}>
+					{Intl.NumberFormat('pt-BR', {
+						style: 'currency',
+						currency: 'BRL'
+					}).format(incident.value)}
+				</Text>
 			</View>
 
 			<View style={styles.contactBox}>
